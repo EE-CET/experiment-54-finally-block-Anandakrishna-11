3d5dcddeba1747d936dc5235847757ed2d55d88e
Anandakrishna <ellu250075@cet.ac.in>
2026-02-21 13:12:03 +0530

    Added FinallyDemo

--- src/FinallyDemo.java
+++ src/FinallyDemo.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 
 public class FinallyDemo {
     
-        // TODO: Read a and b
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
-        // TODO: try block
-        // Perform division
-        // Print "Result: " + result
-        
-        // TODO: catch ArithmeticException e
-        // Print "Error: " + e.getMessage()
-        
-        // TODO: finally block
-        // Print "Finally block executed"
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        try {
+            int result = a / b;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            System.out.println("Finally block executed");
+        }
+
+        sc.close();
     }
 }
